573a0c30d257a789cbb22c1beb8d8bef4a901771
hasahmed <hashemalsaie0457@gmail.com>
2024-02-22 00:36:57 +0300

    strrev

--- strrev.go
+++ strrev.go
@@ -2,9 +2,9 @@ package piscine
 
 func StrRev(s string) string {
 	ary := []rune(s)
-result := ""
-	for i := len(ary)-1; i >= 0 ; i--{
-	result += string(ary[i])
+	result := ""
+	for i := len(ary) - 1; i >= 0; i-- {
+		result += string(ary[i])
 	}
 	return result
 }
