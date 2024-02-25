2dac416f036d59ac2871227e8960597683a85204
hasahmed <hashemalsaie0457@gmail.com>
2024-02-25 21:41:47 +0300

    fibo

--- fibonacci.go
+++ fibonacci.go
@@ -1,10 +1,8 @@
 package piscine
 
 func Fibonacci(index int) int {
-
 	if index >= 3 {
 		return Fibonacci(index-1) + Fibonacci(index-2)
-
 	} else if index == 0 {
 		return 0
 	} else if index < 0 {
