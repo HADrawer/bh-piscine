c3b07a3064bf2bf0706df6f416e5a8a9f4c2de51
hashem <hashemalsaie0457@gmail.com>
2024-02-25 18:21:09 +0300

    iter

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,12 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 {
+	if nb <= 0{
 		return 0
 	}
-	if nb == 1 {
-		return 1
+	result := 1
+	for i := 1 ; i <= nb; i++{
+		result = result * i;
 	}
-	return nb * IterativeFactorial(nb-1)
+	return result
 }
