1628438849479e5041f385b7c319f3eed779e63f
hashem <hashemalsaie0457@gmail.com>
2024-02-25 18:48:16 +0300

    iter power

---
+++ iterativepower.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if(power < 0){
+		return 0
+	}
+	if (nb < 0 || nb > 20){
+		return 0
+	}
+	result := 1
+	
+	for i:= 1 ; i<= power ; i++{
+		result = result * nb
+	}
+	return result
+}
