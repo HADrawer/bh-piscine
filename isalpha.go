58c478e1efa9a1b7f9b645eb03567826a2f3daa6
hasahmed <hashemalsaie0457@gmail.com>
2024-02-26 23:33:17 +0300

    isalpha

---
+++ isalpha.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	result := true
+	str := []rune(s)
+
+	for _, ch := range str {
+		if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '1' && ch <= '9') {
+			result = true
+		} else {
+			result = false
+			return result
+		}
+	}
+	return result
+}
