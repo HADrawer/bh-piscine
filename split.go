dc9c80d7b2c7c464290eaabb049eb316fafa7bbc
root <dev@01-edu.org>
2024-03-04 23:18:18 +0000

    Update 'split.go'

--- split.go
+++ split.go
@@ -1,10 +1,7 @@
 package piscine
 
+import "strings"
+
 func Split(s, sep string) []string {
-	for i := 0; i < len(s); i++ {
-		if Index(s, sep) != -1 {
-			s = s[0:Index(s, sep)] + " " + s[Index(s, sep)+len(sep):]
-		}
-	}
-	return SplitWhiteSpaces(s)
+	return strings.Split(s,sep)
 }
