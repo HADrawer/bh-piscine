0b9f1e5d12b0cc661534e4f976c8764470412701
hasahmed <hashemalsaie0457@gmail.com>
2024-03-05 02:05:59 +0300

    split

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,37 +1,16 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	index := 0
-	count := 0
-	word := ""
-	for i, v := range s {
-		if v == ' ' && s[i+1] != ' ' {
-			count++
+	var result []string
+	var word string
+	for i, c := range s {
+		if c != ' ' && c != '\t' && c != '\n' {
+			word += string(c)
 		}
-	}
-	result := make([]string, count+1)
-	for _, r := range s {
-		if isSeparator(r) {
-			if word != "" {
-				result[index] = word
-				index++
-				word = ""
-			}
-		} else {
-			word += string(r)
+		if ((c == ' ' || c == '\t' || c == '\n') && word != "") || i == len(s)-1 {
+			result = append(result, word)
+			word = ""
 		}
-	}
-	size := 0
-	for z := range result {
-		size++
-		z++
-	}
-	if word != "" {
-		result[size-1] = word
 	}
 	return result
-}
-
-func isSeparator(r rune) bool {
-	return r == ' ' || r == '\t' || r == '\n'
 }
