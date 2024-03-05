3f9a381b24aa9cf6cb9cf67908436ef06071c606
hashem <hashemalsaie0457@gmail.com>
2024-03-05 15:59:56 +0000

    point

--- point/main.go
+++ point/main.go
@@ -13,9 +13,35 @@ func setPoint(ptr *point) {
 }
 
 func main() {
-	str := "x = 42, y = 21"
-	for _, v := range str {
-		z01.PrintRune(v)
+	xStr := "x = "
+	yStr := "y = "
+	points := &point{}
+	setPoint(points)
+	xmassiv := []rune{}
+	ymassiv := []rune{}
+	xVal := points.x
+	yVal := points.y
+	for xVal != 0 {
+		xmassiv = append(xmassiv, rune(xVal%10))
+		xVal /= 10
+	}
+	for _, val := range xStr {
+		z01.PrintRune(rune(val))
+	}
+	for i := len(xmassiv) - 1; i >= 0; i-- {
+		z01.PrintRune(xmassiv[i] + 48)
+	}
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	for yVal != 0 {
+		ymassiv = append(ymassiv, rune(yVal%10))
+		yVal /= 10
+	}
+	for _, val := range yStr {
+		z01.PrintRune(rune(val))
+	}
+	for i := len(ymassiv) - 1; i >= 0; i-- {
+		z01.PrintRune(ymassiv[i] + 48)
 	}
 	z01.PrintRune('\n')
 }
