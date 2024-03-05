8100c1fae80b17f5a72aefcd9d0f106ec67e23e8
hasahmed <hashemalsaie0457@gmail.com>
2024-03-06 02:38:58 +0300

    point

--- point/main.go
+++ point/main.go
@@ -1,10 +1,9 @@
 package main
 
-import "github.com/01-edu/z01"
+import "fmt"
 
 type point struct {
-	x int
-	y int
+	x, y int
 }
 
 func setPoint(ptr *point) {
@@ -13,35 +12,7 @@ func setPoint(ptr *point) {
 }
 
 func main() {
-	xStr := "x = "
-	yStr := "y = "
-	points := &point{}
+	points := new(point)
 	setPoint(points)
-	xmassiv := []rune{}
-	ymassiv := []rune{}
-	xVal := points.x
-	yVal := points.y
-	for xVal != 0 {
-		xmassiv = append(xmassiv, rune(xVal%10))
-		xVal /= 10
-	}
-	for _, val := range xStr {
-		z01.PrintRune(rune(val))
-	}
-	for i := len(xmassiv) - 1; i >= 0; i-- {
-		z01.PrintRune(xmassiv[i] + 48)
-	}
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	for yVal != 0 {
-		ymassiv = append(ymassiv, rune(yVal%10))
-		yVal /= 10
-	}
-	for _, val := range yStr {
-		z01.PrintRune(rune(val))
-	}
-	for i := len(ymassiv) - 1; i >= 0; i-- {
-		z01.PrintRune(ymassiv[i] + 48)
-	}
-	z01.PrintRune('\n')
+	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }
