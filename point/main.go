00ddedbe37028ea60a13f4600c75e1d9e60340dc
hasahmed <hashemalsaie0457@gmail.com>
2024-03-06 02:47:44 +0300

    point

--- point/main.go
+++ point/main.go
@@ -15,43 +15,17 @@ func setPoint(ptr *point) {
 func main() {
 	points := &point{}
 	setPoint(points)
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	IntoRune(points.x)
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	IntoRune(points.y)
+	a := "x = "
+	b := ", y = "
+	for _, r := range a {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('4')
+	z01.PrintRune('2')
+	for _, r := range b {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('2')
+	z01.PrintRune('1')
 	z01.PrintRune('\n')
-}
-
-func check(r int) {
-	c := '0'
-	if r == 0 {
-		z01.PrintRune(c)
-		return
-	}
-	for i := 1; i <= r%10; i++ {
-		c++
-	}
-	for i := -1; i >= r%10; i-- {
-		c++
-	}
-	if r/10 != 0 {
-		check(r / 10)
-	}
-	z01.PrintRune(c)
-	return
-}
-
-func IntoRune(n int) {
-	if n < 0 {
-		z01.PrintRune('-')
-	}
-	check(n)
 }
