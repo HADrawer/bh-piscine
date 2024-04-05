c804ee9668f2ccc5e9b66c79b5fe9a4d557761a9
hasahmed <hashemalsaie0457@gmail.com>
2024-04-05 21:30:01 +0300

    hm

--- point/main.go
+++ point/main.go
@@ -1,19 +1,62 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
+
+//import "fmt"
 
 type point struct {
-	x, y int
+	x int
+	y int
 }
 
 func setPoint(ptr *point) {
+
 	ptr.x = 42
 	ptr.y = 21
 }
 
 func main() {
-	points := new(point)
-
+	points := &point{}
 	setPoint(points)
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	IntoRune(points.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	IntoRune(points.y)
+	z01.PrintRune('\n')
+}
+
+func check(r int) {
+
+	c := '0'
+	if r == 0 {
+		z01.PrintRune(c)
+		return
+	}
+	for i := 1; i <= r%10; i++ {
+		c++
+	}
+	for i := -1; i >= r%10; i-- {
+		c++
+	}
+	if r/10 != 0 {
+		check(r / 10)
+	}
+	z01.PrintRune(c)
+	return
+
+}
+
+func IntoRune(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	check(n)
 }
