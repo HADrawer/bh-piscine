90aeb7737187725b4af7ce012847fdd2136e08e4
hasahmed <hashemalsaie0457@gmail.com>
2024-04-20 23:06:46 +0300

    btree

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,11 +1,14 @@
 package piscine
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-	if root == nil || root.Data == elem {
+	if root == nil {
+		return nil
+	}
+	if elem < root.Data {
+		return BTreeSearchItem(root.Left, elem)
+	} else if elem > root.Data {
+		return BTreeSearchItem(root.Right, elem)
+	} else {
 		return root
 	}
-	if res := BTreeSearchItem(root.Left, elem); res != nil {
-		return res
-	}
-	return BTreeSearchItem(root.Right, elem)
 }
